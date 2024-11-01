35e04ae7544c5e4fe7d6b23259f40a4cedc27f5a
Floyd Johnson <fljohnson3@sisna.com>
2024-11-01 15:55:56 -0400

    Revert "Added second comment for rollback test"
    This reverts commit 4503c34524dd6fac4419051c8dd3667a236836de.

--- src/main/java/com/fouracessoftware/moneylogsxm/TxnViewModel.kt
+++ src/main/java/com/fouracessoftware/moneylogsxm/TxnViewModel.kt
@@ -5,5 +5,4 @@ import androidx.lifecycle.ViewModel
 class TxnViewModel : ViewModel() {
     // TODO: Implement the ViewModel
     /*Test comment here (FLJ, 10/30/24)*/
-    /*A second comment (FLJ, 11/1/24) */
 }
