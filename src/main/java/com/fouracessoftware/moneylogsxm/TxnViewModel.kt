4503c34524dd6fac4419051c8dd3667a236836de
Floyd Johnson <fljohnson3@sisna.com>
2024-11-01 15:46:43 -0400

    Added second comment for rollback test

--- src/main/java/com/fouracessoftware/moneylogsxm/TxnViewModel.kt
+++ src/main/java/com/fouracessoftware/moneylogsxm/TxnViewModel.kt
@@ -5,4 +5,5 @@ import androidx.lifecycle.ViewModel
 class TxnViewModel : ViewModel() {
     // TODO: Implement the ViewModel
     /*Test comment here (FLJ, 10/30/24)*/
+    /*A second comment (FLJ, 11/1/24) */
 }
